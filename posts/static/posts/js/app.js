f5a61c1a58fa491e14a214ff966c519610021dcd
Dmytrieev Denys <dmytriyevdenys@gmail.com>
2022-11-07 23:51:15 +0200

    first promlem

---
+++ posts/static/posts/js/app.js
@@ -0,0 +1,48 @@
+
+const postContainer = document.querySelector('.posts')
+let postService
+
+
+class PostServive {
+
+constructor( posts = []){ 
+   this.posts = posts
+
+}
+}
+class ToHTML {
+
+    paintPost (post) {
+        return `<li>
+        <small> id :${post.id}</small>
+        <small> текст : ${post.title}</small> 
+        </li>
+        `
+    }
+
+    paintPosts (posts = []) {
+        return posts.map(this.paintPost).join('')
+    }
+}
+const toHtml = new ToHTML()
+
+
+function renderPosts (posts) {
+
+    postContainer.innerHTML = toHtml.paintPosts(posts)
+
+}
+
+
+async function startHomePage (){ 
+
+const respone = await fetch ('https://http://127.0.0.1:8000/api/posts')
+const data = await respone.json()
+
+postService = new PostServive(data)
+renderPosts(postService.posts)
+}
+
+
+startHomePage ()
+
