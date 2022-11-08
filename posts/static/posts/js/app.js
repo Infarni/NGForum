5fac5fdc3e90ebf79d16a2f39489ba4839b0fc69
Infarni <mr.infarni@gmail.com>
2022-11-08 17:34:36 +0200

    Add avatars to posts view

--- posts/static/posts/js/app.js
+++ posts/static/posts/js/app.js
@@ -14,14 +14,14 @@ class ToHTML {
 
     paintPost (post) {
         return `<li>
+        <br>
+        <br>
         <img src="${post.author_avatar}" style="height: 50px; width: 50px">
         username : <span>${post.author_username}</span>
         <div><small> id :${post.id}</small>
-        <small> назва : ${post.title}</small></div>
+        назва : ${post.title}</div>
         <div> текст : ${post.text} </div>
         </li>
-        <br>
-        <br>
         `
     }
 
