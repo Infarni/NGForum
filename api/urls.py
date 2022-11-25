f20134d325082af8bd401b8508d67c31b83e095d
Infarni <archdroider@proton.me>
2022-11-25 11:49:03 +0200

    Update API posts

--- api/urls.py
+++ api/urls.py
@@ -4,26 +4,17 @@ from . import views
 urlpatterns = [
     path('posts/', views.posts_no_filters, name='api_posts_no_filters'),
     path(
-        'posts&filter(<int:start>-<int:end>)/',
+        'posts&filter(number=<int:number>)/',
         views.PostsAPIView.as_view(),
         name='api_posts'
     ),
-    path('posts/<int:pk>/', views.PostAPIView.as_view(), name='api_post'),
+    path('posts/<int:id>/', views.PostAPIView.as_view(), name='api_post'),
     path(
-        'posts/<int:pk>/images/',
-        views.UploadPostImageAPIView.as_view(),
-        name='api_upload_image'
-    ),
-    path(
-        'posts/<int:pk>/comment/',
+        'posts/<int:id>/comment/',
         views.CommentAPIView.as_view(),
         name='api_comment'
     ),
-    path(
-        'posts/create/',
-        views.CreatePostAPIView.as_view(),
-        name='api_create_post'
-    ),
-    path('users/', views.UsersAPIView.as_view(), name='api_users'),
-    path('users/<int:pk>/', views.UserAPIView.as_view(), name='api_user'),
+    path('users/', views.users_no_filters, name='api_posts_no_filters'),
+    path('users&filter(number=<int:number>)/', views.UsersAPIView.as_view(), name='api_users'),
+    path('users/<int:id>/', views.UserAPIView.as_view(), name='api_user'),
 ]
--- api/views.py
+++ api/views.py
@@ -7,43 +7,41 @@ from posts.models import *
 
 
 def posts_no_filters(request):
-    return redirect('api_posts', start=0, end=len(Post.objects.all()))
+    return redirect('api_posts', number=1)
+
+def users_no_filters(request):
+    return redirect('api_users', number=1)
 
 
 class PostsAPIView(APIView):
-    def get(self, request, start, end):
+    def get(self, request, number):
+        if number == 1:
+            end = 4 * number
+        else:
+            end = 4 * number - 1
+        start = end - 4
         posts_unformated = Post.objects.order_by('-date')[start:end]
         posts = []
 
         for post in posts_unformated:
             images_unformated = PostImage.objects.filter(post=post)
             images = []
-            comments_unformated = Comment.objects.filter(post=post)
-            comments = []
-
-            for img in images_unformated:
-                images.append({'image': img.image.url})
-
-            for comment in comments_unformated:
-                comments.append(
+            
+            for image in images_unformated:
+                images.append(
                     {
-                        'author_avatar': comment.author.avatar.url,
-                        'author_username': comment.author.username,
-                        'author_id': comment.author.id,
-                        'text': comment.text,
-                        'date': comment.date
+                        "id": image.id,
+                        "image": image.image.url
                     }
                 )
-
             posts.append(
                 {
-                    'author_avatar': post.author.avatar.url,
-                    'author_username': post.author.username,
-                    'id': post.id,
-                    'title': post.title,
-                    'images': images,
-                    'text': post.text,
-                    'comments': comments,
+                    "userId": post.author.id,
+                    "id": post.id,
+                    "title": post.title,
+                    "body": post.text,
+                    "images": images,
+                    "date": post.date
                 }
             )
 
@@ -51,114 +49,58 @@ class PostsAPIView(APIView):
 
 
 class PostAPIView(APIView):
-    def get(self, request, pk):
-        post_unformated = get_object_or_404(Post, pk=pk)
+    def get(self, request, id):
+        post_unformated = get_object_or_404(Post, id=id)
         images_unformated = PostImage.objects.filter(post=post_unformated)
         images = []
-        comments_unformated = Comment.objects.filter(post=post_unformated)
-        comments = []
-
-        for img in images_unformated:
-            images.append({'image': img.image.url})
-
-        for comment in comments_unformated:
-            comments.append(
+        
+        for image in images_unformated:
+            images.append(
                 {
-                    'author_avatar': comment.author.avatar.url,
-                    'author_username': comment.author.username,
-                    'author_id': comment.author.id,
-                    'text': comment.text,
-                    'date': comment.date
+                    "id": image.id,
+                    "image": image.image.url
                 }
             )
-
+        
         post = {
-            'author_avatar': post_unformated.author.avatar.url,
-            'author_username': post_unformated.author.username,
-            'id': post_unformated.id,
-            'title': post_unformated.title,
-            'images': images,
-            'text': post_unformated.text,
-            'comments': comments,
+            "userId": post_unformated.author.id,
+            "id": post_unformated.id,
+            "title": post_unformated.title,
+            "body": post_unformated.text,
+            "images": images,
+            "date": post_unformated.date
         }
 
         return Response(post)
 
 
-class CreatePostAPIView(APIView):
-    def post(self, request):
-        post_object = PostForm(request.POST)
-
-        if post_object.is_valid():
-            instance = post_object.save(commit=False)
-            instance.author = request.user
-            instance.save()
-
-            return Response(
+class CommentAPIView(APIView):
+    def get(self, request, id):
+        post = get_object_or_404(Post, id=id)
+        comments_unformated = Comment.objects.filter(post=post)
+        comments = []
+        
+        for comment in comments_unformated:
+            comments.append(
                 {
-                    'author_avatar': instance.author.avatar.url,
-                    'author_username': instance.author.username,
-                    'author_id': instance.author.id,
-                    'title': instance.title,
-                    'text': instance.text,
-                    'date': instance.date
+                    "userId": comment.author.id,
+                    "id": comment.id,
+                    "body": comment.text,
+                    "date": comment.date
                 }
             )
-
-
-class UploadPostImageAPIView(APIView):
-    def post(self, request, pk):
-        post_object = get_object_or_404(Post, pk=pk)
-        image = PostImageForm(request.POST, request.FILES)
-        if image.is_valid():
-            instance = image.save(commit=False)
-            instance.post = post_object
-            instance.save()
-
-        images_unformated = PostImage.objects.filter(post=post_object)
-        images = []
-
-        for img in images_unformated:
-            images.append(
-                {'url': img.image.url}
-            )
-        return Response(
-            {
-                'post': post_object.id,
-                'images': images
-            }
-        )
-
-
-class CommentAPIView(APIView):
-    def post(self, request, pk):
-        post_object = get_object_or_404(Post, pk=pk)
-        comments = []
-        comment = CommentForm(request.POST)
-
-        if comment.is_valid():
-            instance = comment.save(commit=False)
-            instance.post = post_object
-            instance.author = request.user
-            instance.save()
-
-            for comm in Comment.objects.filter(post=post_object):
-                comments.append(
-                    {
-                        'author_id': comm.author.id,
-                        'author_avatar': comm.author.avatar.url,
-                        'author_username': comm.author.username,
-                        'text': comm.text,
-                        'date': comm.date
-                    }
-                )
-
-            return Response(comments)
+            
+        return Response(comments)
 
 
 class UsersAPIView(APIView):
-    def get(self, request):
-        users_unformated = CustomUser.objects.all()
+    def get(self, request, number):
+        if number == 1:
+            end = 4 * number
+        else:
+            end = 4 * number - 1
+        start = end - 4
+        users_unformated = CustomUser.objects.all()[start:end]
         users = []
 
         for user in users_unformated:
@@ -175,8 +117,8 @@ class UsersAPIView(APIView):
 
 
 class UserAPIView(APIView):
-    def get(self, request, pk):
-        user_unformated = get_object_or_404(CustomUser, pk=pk)
+    def get(self, request, id):
+        user_unformated = get_object_or_404(CustomUser, id=id)
         user = {
             'user': user_unformated.id,
             'username': user_unformated.username,
